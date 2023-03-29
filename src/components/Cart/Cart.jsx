3372144263840111ada1a7dccdf1e2f74302bdc2
ariaan ahmed <you@example.com>
2023-03-29 15:52:29 +0600

    localstorage to ui display data completed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,15 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     // 5% tax hisebe ekhane dhora hocche
     const tax = totalPrice * 5 / 100;
@@ -18,7 +24,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h2>order summary</h2>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${totalPrice.toFixed(2)}</p>
             <p>Total Shipping:${totalShipping.toFixed(2)}</p>
             <p>Tax: ${tax}</p>
